b7b6cd901c7a262658e5d5ef7b069e5cde98b5df
aLaptop <aidenramsey06@gmail.com>
2025-02-03 20:43:04 -0600

    added images, added some database function (need to do it forr drink & movie html as well)

---
+++ index.js
@@ -0,0 +1,64 @@
+
+
+//function displaying the contents for the first no clicked
+function firstYesClick()
+{
+    let elements = document.getElementsByClassName("firstYesClickedHidden")
+    for (let i = 0; i < elements.length; i++) {
+        elements[i].style.display = "block";
+    }
+}
+
+function firstNoClick()
+{
+    
+}
+
+//function displaying the content for secondNoClicked
+function secondNoClicked()
+{
+    let elements = document.getElementsByClassName("secondNoClickedHidden")
+    for (let i = 0; i < elements.length; i++)
+    {
+        elements[i].style.display = "block";
+    }
+}
+
+//function displaying the content for thirdYesClicked
+function thirdYesClicked()
+{
+    let elements = document.getElementsByClassName("thirdYesClickedHidden")
+    for (let i = 0; i < elements.length; i++)
+    {
+        elements[i].style.display = "block";
+    }
+}
+
+
+let clickData = []; //array for the data when clicked
+
+//function for when one of the choices are clicked
+function generateText(event)
+{
+    // Get the clicked button's parent container
+    const container = event.target.closest('.container');
+    // Find the output paragraph inside this container
+    const outputElement = container.querySelector('.output');
+    //Get the text from buttons data attribute
+    const text = event.target.getAttribute('data-text');
+    // Update the output text
+    outputElement.innerText = text;
+
+    // Store data in the array
+    const timestamp = new Date().toLocaleTimeString();
+    clickData.push({ text: text, timestamp: timestamp });
+
+
+    // Print the data to console
+    console.log("Button clicked:", text);
+    console.log("Updated clickData array:", clickData);
+    clickData.forEach((entry, index) => {
+        console.log(`Entry ${index + 1}:`, entry);
+    });
+
+}
